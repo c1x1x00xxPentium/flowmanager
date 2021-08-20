0c2f293a599ef7bee3ce5a054924935ba6815e0f
c1x1x00xxPentium <45236352+c1x1x00xxPentium@users.noreply.github.com>
2021-08-21 01:47:22 +0700

    Update flowmanager.py
    minor fix

--- flowmanager.py
+++ flowmanager.py
@@ -20,6 +20,7 @@ from ryu.controller import ofp_event
 from ryu.controller.handler import HANDSHAKE_DISPATCHER
 from ryu.controller.handler import CONFIG_DISPATCHER
 from ryu.controller.handler import MAIN_DISPATCHER
+from ryu.controller.handler import DEAD_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 
 from ryu.ofproto import ofproto_v1_3
@@ -32,10 +33,16 @@ from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
 from ryu.lib.packet import ether_types
 
-# for topology discovery
-from ryu.topology import event
-from ryu.topology.api import get_all_switch, get_all_link, get_all_host
+from ryu.lib.packet import in_proto
+from ryu.lib.packet import ipv4
+from ryu.lib.packet import icmp
+from ryu.lib.packet import tcp
+from ryu.lib.packet import udp
+from ryu.lib.packet import arp
 
+# for topology discovery
+# from ryu.topology import event
+from ryu.topology.api import get_all_switch, get_all_link, get_all_host
 from webapi import WebApi
 import os
 import sys
@@ -44,20 +51,22 @@ from logging.handlers import WatchedFileHandler
 import time
 import random
 import json
+import re
+import array
 
 PYTHON3 = sys.version_info > (3, 0)
 LOG_FILE_NAME = 'flwmgr.log'
+SNORT_LOG_FILE_NAME = 'snortalert.log'
 print("You are using Python v" + '.'.join(map(str, sys.version_info)))
 
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 from flow_monitor import Tracker
 
-
 class FlowManager(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     _CONTEXTS = {'wsgi': WSGIApplication,
-                 'dpset': dpset.DPSet}
+                 'dpset': dpset.DPSet,}
 
     port_id = {
         "IN_PORT": 0xfffffff8,
@@ -73,8 +82,9 @@ class FlowManager(app_manager.RyuApp):
     MONITOR_PKTIN = False
     MAGIC_COOKIE = 0x00007ab700000000
     logname = 'flwmgr'
+    snortlogname = 'snortalert'
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs): # Init Object
         super(FlowManager, self).__init__(*args, **kwargs)
         wsgi = kwargs['wsgi']
         self.dpset = kwargs['dpset']
@@ -102,17 +112,17 @@ class FlowManager(app_manager.RyuApp):
         }
 
         # Setup logging
-        # cwd = os.getcwd()
         cfd = os.path.dirname(os.path.abspath(__file__))
         self.logfile = os.path.join(cfd, LOG_FILE_NAME)
         self.logger = self.get_logger(self.logname, self.logfile, 'INFO', 0)
+        self.snortlogfile = os.path.join(cfd, SNORT_LOG_FILE_NAME)
 
-    def get_logger(self, logname, logfile, loglevel, propagate):
+    def get_logger(self, logname, logfile, loglevel, propagate): # Init Flowmanager Logger
         """Create and return a logger object."""
         # TODO: simplify
         logger = logging.getLogger(logname)
         logger_handler = WatchedFileHandler(logfile, mode='w')
-        # removed \t%(name)-6s
+        # log_fmt = '%(asctime)s\t%(levelname)-8s\t[line %(lineno)d]\t%(name)s\t%(message)s'
         log_fmt = '%(asctime)s\t%(levelname)-8s\t%(message)s'
         logger_handler.setFormatter(
             logging.Formatter(log_fmt, '%b %d %H:%M:%S'))
@@ -121,6 +131,10 @@ class FlowManager(app_manager.RyuApp):
         logger.setLevel(loglevel)
         return logger
 
+    # ================================================================================
+    #  Utility Method ================================================================
+    # ================================================================================
+    
     def get_switches(self):
         """Return switches."""
         return self.dpset.get_all()
@@ -156,7 +170,17 @@ class FlowManager(app_manager.RyuApp):
                     break
                 lst = line.split('\t')
                 items.append(lst)
-                # items.append(line)
+        return items
+
+    def read_snort_logs(self): # Read snortalert.log (Tested on version 0.2.0)
+        items = []
+        with open(self.snortlogfile, 'r') as my_file:
+            while True:
+                line = my_file.readline()
+                if not line:
+                    break
+                lst = line.split('\t')
+                items.append(lst)
         return items
 
     def get_actions(self, parser, set):
@@ -500,7 +524,7 @@ class FlowManager(app_manager.RyuApp):
     #     dp = self.dpset.get(int(str(dpid), 0))
     #     return self.ofctl.get_flow_stats(dp, self.waiters, flow)
 
-    def get_packet_summary(self, content):
+    def get_packet_summary(self, content): # Default function
         pkt = packet.Packet(content)
         eth = pkt.get_protocols(ethernet.ethernet)[0]
         ethtype = eth.ethertype
@@ -508,6 +532,54 @@ class FlowManager(app_manager.RyuApp):
         src = eth.src
 
         return '(src={}, dst={}, type=0x{:04x})'.format(src, dst, ethtype)
+
+    def get_packet_summary_new(self, content):
+        pkt = packet.Packet(content)
+        eth = pkt.get_protocols(ethernet.ethernet)[0]
+        ethtype = eth.ethertype
+        dst = eth.dst
+        src = eth.src
+        message = '(src_mac={}, dst_mac={}, type=0x{:04x})'.format(src, dst, ethtype)
+
+        try:
+            _ipv4 = pkt.get_protocols(ipv4.ipv4
+            try:
+                _icmp = pkt.get_protocols(icmp.icmp)
+            except Exception as error1:
+                print(error1)
+            
+            try:
+                _tcp = pkt.get_protocols(tcp.tcp)
+            except Exception as error2:
+                print(error2)
+            
+            try:
+                _udp = pkt.get_protocols(udp.udp)
+            except Exception as error3:
+                print(error3)
+        except Exception as error:
+            print(error)
+        finally:
+            return message
+
+        if _ipv4:
+            src = _ipv4.src
+            dst = _ipv4.dst
+            proto = _ipv4.proto
+            message += '\n(ip_src={}, ip_dst={}, ip_proto={})'.format(src,dst, proto)
+        if _icmp:
+            type = _icmp.type
+            code = _icmp.code
+            message += '\n(icmp_code={}, icmp_type={})'.format(type,code)
+        if _tcp:
+            sport = _tcp.src_port
+            dport = _tcp.dst_port
+            message += '\n(tcp_sport={}, tcp_dport={})'.format(sport,dport)
+        if _udp:
+            sport = _udp.src_port
+            dport = _udp.dst_port
+            message += '\n(udp_sport={}, udp_dport={})'.format(sport,dport)
+        return message
 
     def process_meter_upload(self, configlist):
         """Sends meters to the switch to update meter tables.
@@ -570,128 +642,6 @@ class FlowManager(app_manager.RyuApp):
 
         return 'Flows added successfully!'
 
-    ##### Event Handlers #######################################
-
-    @set_ev_cls([  # ofp_event.EventOFPStatsReply,
-        ofp_event.EventOFPDescStatsReply,
-        ofp_event.EventOFPFlowStatsReply,
-        ofp_event.EventOFPAggregateStatsReply,
-        ofp_event.EventOFPTableStatsReply,
-        # ofp_event.EventOFPTableFeaturesStatsReply,
-        ofp_event.EventOFPPortStatsReply,
-        # ofp_event.EventOFPQueueStatsReply,
-        # ofp_event.EventOFPQueueDescStatsReply,
-        ofp_event.EventOFPMeterStatsReply,
-        ofp_event.EventOFPMeterFeaturesStatsReply,
-        ofp_event.EventOFPMeterConfigStatsReply,
-        ofp_event.EventOFPGroupStatsReply,
-        # ofp_event.EventOFPGroupFeaturesStatsReply,
-        ofp_event.EventOFPGroupDescStatsReply,
-        ofp_event.EventOFPPortDescStatsReply,
-    ], MAIN_DISPATCHER)
-    def stats_reply_handler(self, ev):
-        msg = ev.msg
-        dp = msg.datapath
-
-        if dp.id not in self.waiters:
-            return
-        if msg.xid not in self.waiters[dp.id]:
-            return
-        lock, msgs = self.waiters[dp.id][msg.xid]
-        msgs.append(msg)
-
-        flags = dp.ofproto.OFPMPF_REPLY_MORE
-
-        if msg.flags & flags:
-            return
-        del self.waiters[dp.id][msg.xid]
-        lock.set()
-
-        # self.messages.append(msg)
-
-    @set_ev_cls(ofp_event.EventOFPFlowRemoved, MAIN_DISPATCHER)
-    def flow_removed_handler(self, ev):
-        msg = ev.msg
-        dp = msg.datapath
-        ofp = dp.ofproto
-        if msg.reason == ofp.OFPRR_IDLE_TIMEOUT:
-            reason = 'IDLE TIMEOUT'
-        elif msg.reason == ofp.OFPRR_HARD_TIMEOUT:
-            reason = 'HARD TIMEOUT'
-        elif msg.reason == ofp.OFPRR_DELETE:
-            reason = 'DELETE'
-        elif msg.reason == ofp.OFPRR_GROUP_DELETE:
-            reason = 'GROUP DELETE'
-        else:
-            reason = 'unknown'
-
-        # TODO: needs to be of the same format as packet-in
-        # self.logger.info('FlowRemoved\t'
-        #                  'cookie=%d priority=%d reason=%s table_id=%d '
-        #                  'duration_sec=%d duration_nsec=%d '
-        #                  'idle_timeout=%d hard_timeout=%d '
-        #                  'packet_count=%d byte_count=%d match.fields=%s',
-        #                  msg.cookie, msg.priority, reason, msg.table_id,
-        #                  msg.duration_sec, msg.duration_nsec,
-        #                  msg.idle_timeout, msg.hard_timeout,
-        #                  msg.packet_count, msg.byte_count, msg.match)
-
-    @set_ev_cls(ofp_event.EventOFPErrorMsg,
-                [HANDSHAKE_DISPATCHER, CONFIG_DISPATCHER, MAIN_DISPATCHER])
-    def error_msg_handler(self, ev):
-        msg = ev.msg
-
-        # TODO: needs to be of the same format as packet-in
-        # self.logger.error('ErrorMsg\ttype=0x%02x code=0x%02x '
-        #                   'message=%s',
-        #                   msg.type, msg.code, utils.hex_array(msg.data))
-
-    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
-    def packet_in_handler(self, ev):
-        msg = ev.msg
-        dp = msg.datapath
-        ofp = dp.ofproto
-
-        pkt = packet.Packet(msg.data)
-
-        # All packet-in messages are looged except LLDP packets
-        eth = pkt.get_protocol(ethernet.ethernet)
-        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
-            # ignore lldp packet
-            return
-
-        # Monitor packets. Flow entries with cookies take precedance
-        tracked_msg = None
-        if msg.cookie & self.MAGIC_COOKIE == self.MAGIC_COOKIE:
-            # track the packet if it has a magic cookie
-            tracked_msg = self.tracker.track(msg.cookie, pkt)
-        elif not self.MONITOR_PKTIN:
-            # track the packet the global tracking option is enabled
-            tracked_msg = self.tracker.track(self.MAGIC_COOKIE, pkt)
-
-        # Send the tracked message to the interface
-        if tracked_msg:
-            self.rpc_broadcall("update", json.dumps(tracked_msg))
-
-        # Continue the normal processing of Packet_In
-
-        # The reason for packet_in
-        reason_msg = {ofp.OFPR_NO_MATCH: "NO MATCH",
-                      ofp.OFPR_ACTION: "ACTION",
-                      ofp.OFPR_INVALID_TTL: "INVALID TTL"
-                      }
-        reason = reason_msg.get(msg.reason, 'UNKNOWN')
-
-        now = time.strftime('%b %d %H:%M:%S')
-        match = msg.match.items()  # ['OFPMatch']['oxm_fields']
-        log = list(map(str, [now, 'PacketIn', dp.id, msg.table_id, reason, match,
-                        hex(msg.buffer_id), msg.cookie, self.get_packet_summary(msg.data)]))
-        # self.logger.info('\t'.join(log))
-        try:
-            self.rpc_broadcall("log", json.dumps(log))
-        except:
-            pass # avoiding not-serializable objects
-
     def rpc_broadcall(self, func_name, msg):
         from socket import error as SocketError
         from tinyrpc.exc import InvalidReplyError
@@ -701,16 +651,16 @@ class FlowManager(app_manager.RyuApp):
             rpc_server = rpc_client.get_proxy()
             try:
                 getattr(rpc_server, func_name)(msg)
+                self.logger.info('WebSocket connected: %s', rpc_client.ws)
             except SocketError:
-                self.logger.debug('WebSocket disconnected: %s', rpc_client.ws)
+                self.logger.info('WebSocket disconnected: %s', rpc_client.ws)
                 disconnected_clients.append(rpc_client)
             except InvalidReplyError as e:
                 self.logger.error(e)
 
         for client in disconnected_clients:
             self.rpc_clients.remove(client)
-
-    # @set_ev_cls(event.EventSwitchEnter)
+            self.logger.info('Removed : %s', client)
 
     def get_topology_data(self):
         """Get Topology Data
@@ -768,3 +718,128 @@ class FlowManager(app_manager.RyuApp):
             self.tracker.reset(int(id))
 
         return ''
+  
+    # ================================================================================
+    #  Event Handlers ================================================================
+    # ================================================================================
+
+    @set_ev_cls([  # ofp_event.EventOFPStatsReply,
+        ofp_event.EventOFPDescStatsReply,
+        ofp_event.EventOFPFlowStatsReply,
+        ofp_event.EventOFPAggregateStatsReply,
+        ofp_event.EventOFPTableStatsReply,
+        # ofp_event.EventOFPTableFeaturesStatsReply,
+        ofp_event.EventOFPPortStatsReply,
+        # ofp_event.EventOFPQueueStatsReply,
+        # ofp_event.EventOFPQueueDescStatsReply,
+        ofp_event.EventOFPMeterStatsReply,
+        ofp_event.EventOFPMeterFeaturesStatsReply,
+        ofp_event.EventOFPMeterConfigStatsReply,
+        ofp_event.EventOFPGroupStatsReply,
+        # ofp_event.EventOFPGroupFeaturesStatsReply,
+        ofp_event.EventOFPGroupDescStatsReply,
+        ofp_event.EventOFPPortDescStatsReply,
+    ], MAIN_DISPATCHER)
+    def stats_reply_handler(self, ev):
+        msg = ev.msg
+        dp = msg.datapath
+
+        if dp.id not in self.waiters:
+            return
+        if msg.xid not in self.waiters[dp.id]:
+            return
+        lock, msgs = self.waiters[dp.id][msg.xid]
+        msgs.append(msg)
+
+        flags = dp.ofproto.OFPMPF_REPLY_MORE
+
+        if msg.flags & flags:
+            return
+        del self.waiters[dp.id][msg.xid]
+        lock.set()
+
+    @set_ev_cls(ofp_event.EventOFPFlowRemoved, MAIN_DISPATCHER)
+    def flow_removed_handler(self, ev):
+        msg = ev.msg
+        dp = msg.datapath
+        ofp = dp.ofproto
+        if msg.reason == ofp.OFPRR_IDLE_TIMEOUT:
+            reason = 'IDLE TIMEOUT'
+        elif msg.reason == ofp.OFPRR_HARD_TIMEOUT:
+            reason = 'HARD TIMEOUT'
+        elif msg.reason == ofp.OFPRR_DELETE:
+            reason = 'DELETE'
+        elif msg.reason == ofp.OFPRR_GROUP_DELETE:
+            reason = 'GROUP DELETE'
+        else:
+            reason = 'unknown'
+
+        # TODO: needs to be of the same format as packet-in
+        # self.logger.info('FlowRemoved\t'
+        #                  'cookie=%d priority=%d reason=%s table_id=%d '
+        #                  'duration_sec=%d duration_nsec=%d '
+        #                  'idle_timeout=%d hard_timeout=%d '
+        #                  'packet_count=%d byte_count=%d match.fields=%s',
+        #                  msg.cookie, msg.priority, reason, msg.table_id,
+        #                  msg.duration_sec, msg.duration_nsec,
+        #                  msg.idle_timeout, msg.hard_timeout,
+        #                  msg.packet_count, msg.byte_count, msg.match)
+
+    @set_ev_cls(ofp_event.EventOFPErrorMsg,
+                [HANDSHAKE_DISPATCHER, CONFIG_DISPATCHER, MAIN_DISPATCHER])
+    def error_msg_handler(self, ev):
+        msg = ev.msg
+        # TODO: needs to be of the same format as packet-in
+        # self.logger.error('ErrorMsg\ttype=0x%02x code=0x%02x '
+        #                   'message=%s',
+        #                   msg.type, msg.code, utils.hex_array(msg.data))
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def packet_in_handler(self, ev):
+        pass
+        # msg = ev.msg
+        # dp = msg.datapath
+        # ofp = dp.ofproto
+
+        # pkt = packet.Packet(msg.data)
+        # # All packet-in messages are looged except LLDP packets
+        # eth = pkt.get_protocol(ethernet.ethernet)
+        # if eth.ethertype == ether_types.ETH_TYPE_LLDP:
+        #     # ignore lldp packet
+        #     return
+
+        # # All packet-in messages are looged except IPV6 packets
+        # eth = pkt.get_protocol(ethernet.ethernet)
+        # if eth.ethertype == ether_types.ETH_TYPE_IPV6:
+        #     # ignore ipv6 packet
+        #     return
+
+        # # Monitor packets. Flow entries with cookies take precedance
+        # tracked_msg = None
+        # if msg.cookie & self.MAGIC_COOKIE == self.MAGIC_COOKIE:
+        #    # track the packet if it has a magic cookie
+        #    tracked_msg = self.tracker.track(msg.cookie, pkt)
+        # elif not self.MONITOR_PKTIN:
+        #    # track the packet the global tracking option is enabled
+        #    tracked_msg = self.tracker.track(self.MAGIC_COOKIE, pkt)
+        # # Send the tracked message to the interface
+        # if tracked_msg:
+        #    self.rpc_broadcall("update", json.dumps(tracked_msg))
+
+        # # Continue the normal processing of Packet_In
+
+        # # The reason for packet_in
+        # reason_msg = {ofp.OFPR_NO_MATCH: "NO MATCH",
+        #               ofp.OFPR_ACTION: "ACTION",
+        #               ofp.OFPR_INVALID_TTL: "INVALID TTL"
+        #               }
+        # reason = reason_msg.get(msg.reason, 'UNKNOWN')
+
+        # now = time.strftime('%b %d %H:%M:%S')
+        # match = msg.match.items()  # ['OFPMatch']['oxm_fields']
+        # log = list(map(str, [now, 'PacketIn', dp.id, msg.table_id, reason, match,
+        #                 hex(msg.buffer_id), msg.cookie, self.get_packet_summary_new(msg.data)]))
+        # try:
+        #     self.rpc_broadcall("log", json.dumps(log))
+        # except:
+        #     pass # avoiding not-serializable objects
